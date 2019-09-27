e8fe11beb01103829de3d23c0839dc7cb818a7e4
jmac131 <39316253+jmac131@users.noreply.github.com>
2019-09-26 23:35:16 -0300

    Add files via upload

---
+++ atos_challenge.py
@@ -0,0 +1,32 @@
+
+
+import math
+import numpy as np
+
+infile="input.txt"
+
+f=open(infile,'r')
+
+#A=np.zeros((2,2))
+#A[1,1] = xa1-xa0
+#A[1,2] = ya1-ya0
+#A[2,1] = xb1-xb0
+#A[2,2] = yb1-yb0
+
+b=np.zeros(2)
+b[1]=
+x=np.zeros(2)
+print b
+
+
+
+
+
+
+
+
+
+
+
+
+
